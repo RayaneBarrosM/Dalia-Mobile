316216aaa036eb6941551735d8eb2f380f54bc02
RayaneBarrosM <rayane.mendes@fatec.sp.gov.br>
2026-08-25 00:15:44 -0300

    Ajustes pra o botão home não reiniciar no modo errado

--- app/src/main/java/com/example/dalia2/ui/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/dalia2/ui/components/BottomNavigationBar.kt
@@ -11,6 +11,7 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 @Composable
 fun BottomNavigationBar(
     navController: NavController,
+    isPregnantMode: Boolean,
     modifier: Modifier = Modifier
 ) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
@@ -23,17 +24,21 @@ fun BottomNavigationBar(
         windowInsets = NavigationBarDefaults.windowInsets
     ) {
         Destination.items.forEach { destination ->
-            val isSelected = currentRoute == destination.route
+            val isSelected = isDestinationSelected(destination, currentRoute)//Para deixar a aba em destaque e diferenciar as telas de home e calendar
 
             NavigationBarItem(
                 selected = isSelected,
                 onClick = {
-                    navController.navigate(destination.route) {
-                        popUpTo(navController.graph.startDestinationId) {
-                            saveState = true
+                    val targetRoute = getTargetRoute(destination, isPregnantMode)
+
+                    if (currentRoute != targetRoute) { //Impede que a tela reinicie no modo errado
+                        navController.navigate(targetRoute) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
                     }
                 },
                 icon = {
@@ -65,5 +70,22 @@ fun BottomNavigationBar(
                 )
             )
         }
+    }
+}
+
+// Funções Auxiliares de Decisão de Rota para ajudar na persistencia
+private fun getTargetRoute(destination: Destination, isPregnantMode: Boolean): String {
+    return when (destination) {
+        Destination.Home -> if (isPregnantMode) "homePregnant" else "home"
+        Destination.Calendar -> if (isPregnantMode) "calendarPregnant" else "calendar"
+        else -> destination.route
+    }
+}
+
+private fun isDestinationSelected(destination: Destination, currentRoute: String?): Boolean {
+    return when (destination) {
+        Destination.Home -> currentRoute == "home" || currentRoute == "homePregnant"
+        Destination.Calendar -> currentRoute == "calendar" || currentRoute == "calendarPregnant"
+        else -> currentRoute == destination.route
     }
 }
